10a0d8cde7e9647dad86491a3bfd1f5bd7792c1b
LucasSimplicio <103124268+LucasSimplicio@users.noreply.github.com>
2023-07-01 00:41:29 -0300

    Restos dos últimos arquivos.
    Resto dos últimos arquivos do site.

--- codigo/src/htmlModules/header.js
+++ codigo/src/htmlModules/header.js
@@ -3,15 +3,18 @@ document.getElementById("header").innerHTML +=
 <div id="loginModal" class="w3-modal">
         <div id="loginForm">
             <span onclick="closeLoginModal()" class="w3-button close">&times;</span>
+            <span onclick="closeLoginModal2()" class="w3-button close">&times;</span>
             <h3>Bem-vindo!</h3>
             
             <label for="nomeRegisterInput">Nome:</label>
             <input type="text" id="nomeLoginInput" class="registerForm">
 
             <label for="senhaRegisterInput">Senha:</label>
-            <input type="text" id="" class="loginForm">
+            <input type="password" id="" class="loginForm" maxlength="8" required>
             <button type="button" id="loginButton" onclick="login()" class="w3-button"">Entrar</button>
-            <button onclick="openRegisterForm()" type="button" class="w3-button">Não possui conta? Registre-se!</button>
+            <p>Não possui conta? Registre-se!</p>
+            <button onclick="openRegisterForm()" type="button" class="w3-button">Como cliente!</button>
+            <button onclick="openRegisterForm2()" type="button" class="w3-button">Como produtor!</button>
         </div>
 
         <div id="registerForm">
@@ -22,16 +25,50 @@ document.getElementById("header").innerHTML +=
             <label for="nomeRegisterInput">E-Mail:</label>
             <input type="text" id="emailRegisterInput" class="registerForm">
 
-            <label for="senhaRegisterInput">Senha:</label>
-            <input type="text" id="senhaRegisterForm" class="registerForm">
+            <label for="nomeRegisterInput">Celular:</label>
+            <input type="text" id="celularRegisterInput" class="registerForm">
+
+            <label for="nomeRegisterInput">Endereço:</label>
+            <input type="text" id="enderecoRegisterInput" class="registerForm">
+
+            <label for="senhaRegisterInput">Senha (Máximo de 8 caracteres):</label>
+            <input type="password" id="senhaRegisterForm" class="registerForm" maxlength="8" required>
 
             <label for="confirmarSenhaRegisterInput">Confirmar Senha:</label>
-            <input type="text" id="confirmarSenhaRegisterInput" class="registerForm">
+            <input type="password" id="confirmarSenhaRegisterInput" class="registerForm" maxlength="8" required>
 
             <label for="cpfRegisterInput">CPF:</label>
             <input type="text" id="cpfRegisterInput" class="registerForm">
 
-            <button type="button" id="registerButton" onclick="register()" class="w3-button"">Registrar!</button>
+            <button type="button" id="registerButton" onclick="enviarEmailConfirmacaoCadastro()" class="w3-button"">Registrar!</button>
+        </div>
+        <div id="registerForm2">
+            <span onclick="closeLoginModal2()" class="w3-button close">&times;</span>
+            <label for="nomeRegisterInput">Nome:</label>
+            <input type="text" id="nomeRegisterInput" class="registerForm2">
+
+            <label for="nomeRegisterInput">E-Mail:</label>
+            <input type="text" id="emailRegisterInput" class="registerForm2">
+
+            <label for="nomeRegisterInput">Celular:</label>
+            <input type="text" id="celularRegisterInput" class="registerForm2">
+
+            <label for="nomeRegisterInput">Endereço:</label>
+            <input type="text" id="enderecoRegisterInput" class="registerForm2">
+
+            <label for="nomeRegisterInput">Nome da loja:</label>
+            <input type="text" id="nomelojaRegisterInput" class="registerForm2">
+
+            <label for="senhaRegisterInput">Senha (Máximo de 8 caracteres):</label>
+            <input type="password" id="senhaRegisterForm" class="registerForm2" maxlength="8" required>
+
+            <label for="confirmarSenhaRegisterInput">Confirmar Senha:</label>
+            <input type="password" id="confirmarSenhaRegisterInput" class="registerForm2" maxlength="8" required>
+
+            <label for="cpfRegisterInput">CPF:</label>
+            <input type="text" id="cpfRegisterInput" class="registerForm2">
+
+            <button type="button" id="registerButton" onclick="enviarEmailConfirmacaoCadastro()" class="w3-button"">Registrar!</button>
         </div>
     </div>    
     
@@ -39,25 +76,22 @@ document.getElementById("header").innerHTML +=
     <div class="w3-sidebar w3-bar-block w3-border-right" style="display:none" id="sideBar">
         <img class="w3-bar-item logo" src="../images/egg-fried.svg" alt="logo">
         <button onclick="closeSideBar()" class="w3-bar-item w3-large">Close &times;</button>
-        <a href="#" class="w3-bar-item">Home</a>
-        <a href="#" class="w3-bar-item">Quem Somos</a>
-        <a href="#" class="w3-bar-item">F.A.Q</a>
-        <a href="#" class="w3-bar-item">Link 3</a>
+        <a href="index.html" class="w3-bar-item">Home</a>
+        <a href="quemSomos.html" class="w3-bar-item">Quem Somos</a>
+        <a href="faq.html" class="w3-bar-item">F.A.Q</a>
     </div>
 
     <header>
-        <div class="w3-red" id="myNavbar">
+        <div class="" id="myNavbar">
             <div>
                 <img class="logo" src="../images/egg-fried.svg" alt="logo">
-                <a href="#">TESTE</a>
-                <a href="#" class="w3-hide-small">Home</a>
-                <a href="#" class="w3-hide-small">Quem Somos</a>
-                <a href="#" class="w3-hide-small">F.A.Q</a>
-                <a href="#" class="w3-hide-small">Link 3</a>                
+                <a href="index.html" class="w3-hide-small">Home</a>
+                <a href="quemSomos.html" class="w3-hide-small">Quem Somos</a>
+                <a href="faq.html" class="w3-hide-small">F.A.Q</a>
             </div>
             <div>
                 <img class="imgUser w3-hide-small" src="../images/person-circle.svg" alt="imgUser" onclick="openLoginModal()">
-                <p class="w3-hide-small">Registre-se ou faça login</p>
+                <p class="w3-hide-small" onclick="openLoginModal()">Registre-se ou faça login</p>
                 <a href="javascript:void(0)" class="w3-bar-item w3-right w3-hide-large w3-hide-medium" onclick="openLoginModal()">&#9776;</a>
             </div>
         </div>
--- codigo/src/scripts/util.js
+++ codigo/src/scripts/util.js
@@ -20,3 +20,14 @@ function openSideBar() {
       document.getElementById("loginForm").style.display = "none";
       document.getElementById("registerForm").style.display = "flex";
   }
+
+  function closeLoginModal2(){
+    document.getElementById("loginForm").style.display = "flex";
+    document.getElementById("registerForm2").style.display = "none";
+    document.getElementById("loginModal").style.display = "none";
+}
+
+function openRegisterForm2(){
+    document.getElementById("loginForm").style.display = "none";
+    document.getElementById("registerForm2").style.display = "flex";
+}
